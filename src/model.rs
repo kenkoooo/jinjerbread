4e494f67b82a765aa2819b5c2f1beed887638c59
kenkoooo <kenkou.n@gmail.com>
2018-11-27 16:16:28 +0900

    Added test

--- src/model.rs
+++ src/model.rs
@@ -1,11 +1,11 @@
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq)]
 pub struct LoginResult {
     pub code: usize,
     pub message: String,
     pub data: LoginData,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq)]
 pub struct LoginData {
     pub id: String,
     pub username: String,
@@ -73,5 +73,66 @@ impl TimeCard {
             check: check,
             type_swipe: "PC".to_owned(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn serialize_test() {
+        assert_eq!(
+            serde_json::to_string(&TimeCard::new(Check::In)).unwrap(),
+            r#"{"comment":"","night_mode":"0","type":"check_in","type_swipe":"PC"}"#
+        );
+        assert_eq!(
+            serde_json::to_string(&LoginInfo {
+                company_code: 1234,
+                email: "mail@example.com".to_owned(),
+                password: "password".to_owned(),
+                remember_me: true
+            })
+            .unwrap(),
+            r#"{"company_code":"1234","password":"password","email":"mail@example.com","remember_me":true}"#
+        );
+    }
+
+    #[test]
+    fn deserialize_test() {
+        let json = r#"
+        {
+            "code":200,
+            "message":"msg",
+            "data":{
+                "id": "id",
+                "username": "username",
+                "name": "name",
+                "token": "token",
+                "php_token": "php_token",
+                "shop_id": "shop_id",
+                "shop_name": "shop_name",
+                "company_id": "company_id",
+                "max_break_time": 100
+            }
+        }"#;
+        assert_eq!(
+            serde_json::from_str::<LoginResult>(json).unwrap(),
+            LoginResult {
+                code: 200,
+                message: "msg".to_owned(),
+                data: LoginData {
+                    id: "id".to_owned(),
+                    username: "username".to_owned(),
+                    name: "name".to_owned(),
+                    token: "token".to_owned(),
+                    php_token: "php_token".to_owned(),
+                    shop_id: "shop_id".to_owned(),
+                    shop_name: "shop_name".to_owned(),
+                    company_id: "company_id".to_owned(),
+                    max_break_time: 100
+                }
+            }
+        );
     }
 }
